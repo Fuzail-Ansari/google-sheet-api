e9a52b4916d9e5715e5501d20731bf152b566373
Fuzail-Ansari <102733857+Fuzail-Ansari@users.noreply.github.com>
2022-04-02 06:27:51 -0700

    rest api
    sheet with rest api

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { google } = require("googleapis");
 
 const app = express();
+
 app.use(express.json());
 
 require("dotenv").config();
@@ -31,23 +32,40 @@ app.get("/sheet", async (req, res) => {
       range: "Sheet1!A:B",
     });
 
-    // write rows for spreadsheet
+    res.send(getRows.data);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-    await googleSheets.spreadsheets.values.append({
+app.post("/add", async (req, res) => {
+  try {
+    const { request, name } = req.body;
+    console.log(req.body, "req.body");
+    const auth = new google.auth.GoogleAuth({
+      keyFile: "credentials.json",
+
+      scopes: "https://www.googleapis.com/auth/spreadsheets",
+    });
+    // client instance for auth
+    const client = await auth.getClient();
+    // instance of google sheet api
+    const googleSheets = google.sheets({ version: "v4", auth: client });
+    const spreadsheetId = "1PijkbGM0eP2s4azFn02ibTJp6s6t9WOf21a8rbh4exc";
+
+    //   write rows for spreadsheet
+
+    const data = await googleSheets.spreadsheets.values.append({
       auth,
       spreadsheetId,
       range: "Sheet1!A:B",
       valueInputOption: "USER_ENTERED",
       resource: {
-        values: [
-          ["Make a Row", "test"],
-          ["Make a second Row", "test2"],
-          ["Make a thrid Row", "test3"],
-        ],
+        values: [[request, name]],
       },
     });
-    console.log(getRows, "metasa");
-    res.send(getRows.data);
+    console.log(data, "data");
+    res.send("success");
   } catch (err) {
     console.log(err);
   }
